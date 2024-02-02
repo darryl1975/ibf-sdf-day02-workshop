84c3f3b21847ae62ecfcdd1bb23729a49844da58
darryl1975 <darryl1975@hotmail.com>
2024-02-02 16:38:34 +0800

    added BankAccount and FixedDepositAccount

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to SDF Day02 Workshop Test");
+
+        // Your code here...
+        
     }
 }
